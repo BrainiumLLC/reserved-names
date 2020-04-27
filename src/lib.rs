ce73960e15f6877078aacaca5ec9a50a35bfae61
Francesca Plebani <francesca@brainiumstudios.com>
2020-04-27 11:28:25 -0700

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+
+//! Taken from: https://github.com/rust-lang/cargo/blob/5fe8ab57e2a88ccaaab0821c306203eb19edf8fd/src/cargo/util/restricted_names.rs
+
+pub static KEYWORDS: &'static [&'static str] = &[
+    "Self", "abstract", "as", "async", "await", "become", "box", "break", "const", "continue",
+    "crate", "do", "dyn", "else", "enum", "extern", "false", "final", "fn", "for", "if", "impl",
+    "in", "let", "loop", "macro", "match", "mod", "move", "mut", "override", "priv", "pub", "ref",
+    "return", "self", "static", "struct", "super", "trait", "true", "try", "type", "typeof",
+    "unsafe", "unsized", "use", "virtual", "where", "while", "yield",
+];
+
+pub static WINDOWS: &'static [&'static str] = &[
+    "con", "prn", "aux", "nul", "com1", "com2", "com3", "com4", "com5", "com6", "com7", "com8",
+    "com9", "lpt1", "lpt2", "lpt3", "lpt4", "lpt5", "lpt6", "lpt7", "lpt8", "lpt9",
+];
+
+pub static ARTIFACTS: &'static [&'static str] = &["deps", "examples", "build", "incremental"];
